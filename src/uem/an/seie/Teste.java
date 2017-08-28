b1acc973b3dcb514aee6e2a168b08f557a19b29e
Jose Seie <joseeugenioseie@hotmail.com>
2017-08-28 19:47:21 +0200

    Primeiro commit no projecto

---
+++ src/uem/an/seie/Teste.java
@@ -0,0 +1,12 @@
+
+package uem.an.seie;
+
+/**
+ * Esta vai ser a classe Teste, que vai testar o metodo de Gauss Jordan sem a parte grafica
+ * @author José Seie
+ */
+public class Teste {
+    
+    
+    
+}
